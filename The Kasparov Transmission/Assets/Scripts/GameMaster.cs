ab359eedd59d4f6d6056eef9b1c78a23536821ec
BurberryC06 <christianburberry@gmail.com>
2018-01-27 14:51:48 +0000

    Adding repurposed (not yet used) code.
    - Added repurposed save / load data 'gamemaster'.
    - Added repurposed level selection / loading script.
    - Added Load scene. Load scene should be the startup scene and will
    persist a gameobject for the abovementioned scripts.

---
+++ The Kasparov Transmission/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameMaster : MonoBehaviour {
+
+	//Global access instance.
+	public static GameMaster instance;
+
+	//Enum of what scenes are currently active.
+	public enum ActiveScene 
+	{
+		MainMenu = -1,
+		LevelSelect = 0,
+		GameLevel_1 = 1,
+		GameLevel_2 = 2,
+		GameLevel_3 = 3,
+		GameLevel_4 = 4,
+		GameLevel_5 = 5,
+		GameLevel_6 = 6,
+		GameLevel_7 = 7,
+		GameLevel_8 = 8,
+		GameLevel_9 = 9,
+		GameLevel_10 = 10
+	};
+
+	[HideInInspector]
+	public ActiveScene activeScene;
+
+	[HideInInspector]
+	public bool playTutorial;
+
+	[HideInInspector]
+	public bool MainMenuSelectionReturn = false;
+
+	//Data the game loads from file and used by other parts of the game.
+	[HideInInspector]
+	public GameSessionInfo GameSessionData { get; private set; }
+
+	[SerializeField]
+	private SceneLoader Loader;
+
+	private void InstantiateSessionData() 
+	{
+		GameSessionData = new GameSessionInfo();
+	}
+
+	void Awake() 
+	{
+		activeScene = ActiveScene.MainMenu;
+		instance = this;
+	}
+
+	public void SwapScenes() 
+	{
+		Loader.StartLoadNewCoroutine();
+	}
+
+	public void SaveData( ref GameSessionInfo sDataToSave ) 
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/playerHighscores.dat");
+		PlayerData data = new PlayerData();
+		
+		//TODO: Set elements of PlayerData to be saved to file from GameSessionInfo.
+
+		bf.Serialize(file, data);
+		file.Close();
+	}
+
+	public void LoadData() 
+	{
+		if(File.Exists(Application.persistentDataPath + "/playerHighscores.dat")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/playerHighscores.dat", FileMode.Open);
+			PlayerData data = (PlayerData) bf.Deserialize(file);
+			file.Close();
+
+			//TODO: Get data from PlayerData and store it.
+		}
+	}
+}
+
+//Simple data holding class for menu population and highscore tracking.
+public class GameSessionInfo 
+{
+	//TODO: Add data contents
+}
+
+//Savedata format class.
+[Serializable]
+class PlayerData 
+{
+	//Add what data to be saved / loaded by filesystem here.
+}
---
+++ The Kasparov Transmission/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour 
+{
+	private WaitForEndOfFrame mWaitForEndOfFrame;
+
+	public bool Loading { get; private set; }
+
+	void Awake()
+	{
+		mWaitForEndOfFrame = new WaitForEndOfFrame();
+        Loading = true;
+		DontDestroyOnLoad(transform.gameObject);
+		LoadInitialScene();
+    }
+
+	public void StartLoadNewCoroutine() 
+	{
+		StartCoroutine(LoadNewScene());
+	}
+
+	private void LoadInitialScene() 
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	private IEnumerator LoadNewScene() 
+	{
+		Loading = true;
+		string sceneName = "";
+		switch(GameMaster.instance.activeScene) 
+		{
+			case GameMaster.ActiveScene.MainMenu:
+				sceneName = "MainMenu";
+				break;
+			case GameMaster.ActiveScene.LevelSelect:
+				sceneName = "LevelSelect";
+				break;
+			default:
+				//Numbered level
+				sceneName = string.Concat("GameLevel_", GameMaster.instance.activeScene);
+				break;
+		}
+
+		AsyncOperation ao = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+		if( ao != null ) 
+		{
+			while(!ao.isDone) 
+			{
+				yield return mWaitForEndOfFrame;
+			}
+		}
+		yield return mWaitForEndOfFrame;
+		Loading = false;
+	}
+}
