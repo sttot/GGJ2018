49145a7a19f6a771d89bf8268a508bb2fd03a8cd
fffirstonline <fffirstonline@gmail.com>
2018-01-28 07:28:45 +0000

    Fixes inside code after merge

--- The Kasparov Transmission/Assets/Scripts/Player_Movement.cs
+++ The Kasparov Transmission/Assets/Scripts/Player_Movement.cs
@@ -117,7 +117,7 @@ public class Player_Movement : MonoBehaviour {
 		Vector3[] av3MovementDirections = { Vector3.forward, Vector3.back, Vector3.left, Vector3.right };
 		for ( int iLoop = 0; iLoop < apszMovementKeys.Length; ++iLoop ) 
 		{
-			if ( Input.GetKeyDown( apszMovementKeys[iLoop].ToString() ) )
+			if ( Input.GetKey( apszMovementKeys[iLoop].ToString() ) )
 			{
 				//Get list of 'Bot' children & Get the reordered list by direction
 				GroupMovement cGMovementComp = gameObject.GetComponent<GroupMovement>();
